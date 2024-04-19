7bdd34f139df80c3e038c14c26aaef5f3829cc29
SamuraiYD <167268922+SamuraiYD@users.noreply.github.com>
2024-04-19 16:20:55 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,20 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18{
+	
+	public void setGivenName( ) {
+		
+		this.givenName="花子";
+	      
+	   }
+	
+ //抽象メソッドに具体的な処理を記述	する
+	public void eachIntroduce() {
+    
+		System.out.println("趣味は読書です");
+ }
+	
+			
+	}
+	
+
