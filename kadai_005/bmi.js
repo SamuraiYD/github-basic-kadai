defd50d7f2823ac80fe4e04e5c35747941b4ebb6
SamuraiYD <167268922+SamuraiYD@users.noreply.github.com>
2024-05-30 13:17:22 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,7 @@
 let wheitNumber
 let otherNumber
+let bmiNumber
 wheitNumber = 68
 otherNumber = 1.7*1.7
-console.log(wheitNumber/otherNumber);
+bmiNumber = wheitNumber/otherNumber
+console.log(bmiNumber);
